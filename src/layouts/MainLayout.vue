f36c85c46fca5dc9c2f61b21bd8e524cc96e5d8d
Guilherme de Carvalho <gui.ifsp11@gmail.com>
2020-09-12 13:38:16 -0300

    feat: finalize section 3

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+      class="bg-white text-grey-10"
+      bordered
+    >
+      <q-toolbar class="constrain">
+        <q-btn
+          to="/camera"
+          class="large-screen-only q-mr-sm"
+          icon="eva-camera-outline"
+          size="18px"
+          flat
+          round
+          dense
+        />
+        <q-separator
+          class="large-screen-only"
+          vertical
+          spaced
+        />
+        <q-toolbar-title
+          to="/"
+          class="text-grand-hotel"
+        >
+          Quasagram
+        </q-toolbar-title>
+        <q-btn
+          to="/"
+          icon="eva-home-outline"
+          class="large-screen-only"
+          size="18px"
+          flat
+          round
+          dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer
+      class="bg-white small-screen-only"
+      bordered
+    >
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+        v-model="tab"
+        >
+        <q-route-tab
+          to="/"
+          icon="eva-home-outline"
+        />
+        <q-route-tab
+          to="/camera"
+          icon="eva-camera-outline"
+        />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  components: {},
+  data () {
+    return {
+      tab: "home"
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 77px
+  .q-toolbar__title
+    font-size: 30px
+    font-weight: bold
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+</style>
